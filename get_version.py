24b93e76881d1020b187d10a3505c8146187e1e2
MattTJung <124365945+MattTJung@users.noreply.github.com>
2023-11-22 16:16:17 +0100

    fix get_version.py

--- get_version.py
+++ get_version.py
@@ -1,5 +1,5 @@
 import rdflib
 g = rdflib.Graph()
-g.parse(location='ontology.ttl')
+g.parse(location='main/ontology.ttl')
 qres = g.query(query_object='PREFIX owl: <http://www.w3.org/2002/07/owl#>\nSELECT ?o WHERE {\n\t<https://example.org/example-ontology-stub> owl:versionInfo ?o .\n}\n')
 print(qres[0].o)
